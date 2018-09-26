ca7ff8c4db8427d85fee76dd542e20f6bd120c52
JosephTTran <42635012+JosephTTran@users.noreply.github.com>
2018-09-25 21:04:45 -0700

    rearrange

--- models/Post.php
+++ models/Post.php
@@ -19,6 +19,32 @@ class Post
     $this->conn = $db;
   }
 
+  // Create Post
+  public function create()
+  {
+    $query = 'INSERT INTO ' . $this->table . 
+        ' SET
+            title = :title,
+            body = :body,
+            author = :author,
+            category_id = :category_id';
+    
+    $stmt = $this->conn->prepare($query);
+
+    $stmt->bindParam(':title', $this->title);
+    $stmt->bindParam(':body', $this->body);
+    $stmt->bindParam(':author', $this->author);
+    $stmt->bindParam(':category_id', $this->category_id);
+
+    if ($stmt->execute()) {
+      return true;
+    }
+    
+    printf("Error: %s.\n, $stmt->error");
+
+    return false;
+  }
+  
   // Get Posts
   public function read()
   {
@@ -46,6 +72,7 @@ class Post
     return $stmt;
   }
 
+  // Get single post
   public function readSingle()
   {
     $query = 'SELECT
@@ -83,28 +110,4 @@ class Post
     $this->category_name = $row['category_name'];
   }  
 
-  public function create()
-  {
-    $query = 'INSERT INTO ' . $this->table . 
-        ' SET
-            title = :title,
-            body = :body,
-            author = :author,
-            category_id = :category_id';
-    
-    $stmt = $this->conn->prepare($query);
-
-    $stmt->bindParam(':title', $this->title);
-    $stmt->bindParam(':body', $this->body);
-    $stmt->bindParam(':author', $this->author);
-    $stmt->bindParam(':category_id', $this->category_id);
-
-    if ($stmt->execute()) {
-      return true;
-    }
-    
-    printf("Error: %s.\n, $stmt->error");
-
-    return false;
-  }
 }
